d59f9224b10cc89f2b95b760962a4f613c4284d6
Shon M <him@shonm.com>
2019-05-30 12:28:41 -0600

    Pay attention to array sanitization by key for request data

--- lib/Raven/Processor/SanitizeDataProcessor.php
+++ lib/Raven/Processor/SanitizeDataProcessor.php
@@ -1,88 +1,88 @@
 <?php
 
 /*
- * This file is part of Raven.
- *
- * (c) Sentry Team
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* This file is part of Raven.
+*
+* (c) Sentry Team
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 /**
- * Asterisk out passwords from password fields in frames, http,
- * and basic extra data.
- *
- * @package raven
- */
+* Asterisk out passwords from password fields in frames, http,
+* and basic extra data.
+*
+* @package raven
+*/
 class Raven_Processor_SanitizeDataProcessor extends Raven_Processor
 {
     const MASK = self::STRING_MASK;
     const FIELDS_RE = '/(authorization|password|passwd|secret|password_confirmation|card_number|auth_pw)/i';
     const VALUES_RE = '/^(?:\d[ -]*?){13,16}$/';
-
+    
     protected $fields_re;
     protected $values_re;
     protected $session_cookie_name;
-
+    
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public function __construct(Raven_Client $client)
     {
         parent::__construct($client);
-
+        
         $this->fields_re = self::FIELDS_RE;
         $this->values_re = self::VALUES_RE;
         $this->session_cookie_name = ini_get('session.name');
     }
-
+    
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public function setProcessorOptions(array $options)
     {
         if (isset($options['fields_re'])) {
             $this->fields_re = $options['fields_re'];
         }
-
+        
         if (isset($options['values_re'])) {
             $this->values_re = $options['values_re'];
         }
     }
-
+    
     /**
-     * Replace any array values with our mask if the field name or the value matches a respective regex
-     *
-     * @param mixed $item       Associative array value
-     * @param string $key       Associative array key
-     */
+    * Replace any array values with our mask if the field name or the value matches a respective regex
+    *
+    * @param mixed $item       Associative array value
+    * @param string $key       Associative array key
+    */
     public function sanitize(&$item, $key)
     {
         if (empty($item)) {
             return;
         }
-
+        
         if (preg_match($this->values_re, $item)) {
             $item = self::STRING_MASK;
         }
-
+        
         if (empty($key)) {
             return;
         }
-
+        
         if (preg_match($this->fields_re, $key)) {
             $item = self::STRING_MASK;
         }
     }
-
+    
     public function sanitizeException(&$data)
     {
         foreach ($data['exception']['values'] as &$value) {
             $this->sanitizeStacktrace($value['stacktrace']);
         }
     }
-
+    
     public function sanitizeHttp(&$data)
     {
         $http = &$data['request'];
@@ -92,11 +92,33 @@ class Raven_Processor_SanitizeDataProcessor extends Raven_Processor
                 $cookies[$this->session_cookie_name] = self::STRING_MASK;
             }
         }
+        
         if (!empty($http['data']) && is_array($http['data'])) {
-            array_walk_recursive($http['data'], array($this, 'sanitize'));
+            // array_walk_recursive($http['data'], array($this, 'sanitize'));
+            
+            $this->sanitizeRecursive($http['data']);
         }
     }
-
+    
+    private function sanitizeRecursive(array &$item)
+    {
+        // array_walk_recursive does not receive arrays, only scalars, so anything inside an
+        // array with a key that *would have* been sanitized will not be. for example:
+        // [password => yes] would have been sanitized while [password => [yes]] would NOT have been
+        // this loop will sanitize both, changing [password => [yes]] into [password => ******]
+        // (scrubbing ALL CHILDREN out of existence)
+        foreach ($item as $key => &$value) {
+            // First pass for keys. This wipes out every nested item, if the parent is sanitized
+            // An improvement to this: wiping out only the values of all children
+            $this->sanitize($value, $key);
+            
+            // Second pass when parents don't match aka the usual
+            if (is_array($value)) {
+                $this->sanitizeRecursive($value);
+            }
+        }
+    }
+    
     public function sanitizeStacktrace(&$data)
     {
         foreach ($data['frames'] as &$frame) {
@@ -106,10 +128,10 @@ class Raven_Processor_SanitizeDataProcessor extends Raven_Processor
             array_walk_recursive($frame['vars'], array($this, 'sanitize'));
         }
     }
-
+    
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public function process(&$data)
     {
         if (!empty($data['exception'])) {
@@ -125,34 +147,34 @@ class Raven_Processor_SanitizeDataProcessor extends Raven_Processor
             array_walk_recursive($data['extra'], array($this, 'sanitize'));
         }
     }
-
+    
     /**
-     * @return string
-     */
+    * @return string
+    */
     public function getFieldsRe()
     {
         return $this->fields_re;
     }
-
+    
     /**
-     * @param string $fields_re
-     */
+    * @param string $fields_re
+    */
     public function setFieldsRe($fields_re)
     {
         $this->fields_re = $fields_re;
     }
-
+    
     /**
-     * @return string
-     */
+    * @return string
+    */
     public function getValuesRe()
     {
         return $this->values_re;
     }
-
+    
     /**
-     * @param string $values_re
-     */
+    * @param string $values_re
+    */
     public function setValuesRe($values_re)
     {
         $this->values_re = $values_re;
